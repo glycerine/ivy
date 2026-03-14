b2d83eeb794b5147fda0005cabb3f8dfd4347f7f
Jason E. Aten, Ph.D. <jason@devnull>
2026-03-14 02:51:49 -0300

    adding tests

---
+++ logic/formula_test.go
@@ -0,0 +1,433 @@
+package logic
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestEqValid(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	X, _ := NewVar("X", S)
+	Y, _ := NewVar("Y", S)
+	eq, err := NewEq(X, Y)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if eq.String() != "(X == Y)" {
+		t.Errorf("String() = %q, want %q", eq.String(), "(X == Y)")
+	}
+	if !SortEqual(eq.NodeSort(), Boolean) {
+		t.Error("Eq sort should be Boolean")
+	}
+}
+
+func TestEqDifferentSorts(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	T := &UninterpretedSort{Name: "T"}
+	X, _ := NewVar("X", S)
+	Y, _ := NewVar("Y", T)
+	_, err := NewEq(X, Y)
+	if err == nil {
+		t.Error("Expected error for different sorts")
+	}
+	var se *SortError
+	if !errors.As(err, &se) {
+		t.Error("Expected SortError")
+	}
+}
+
+func TestEqHigherOrder(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	fs := mustFS(t, S, Boolean)
+	X, _ := NewVar("X", fs)
+	Y, _ := NewVar("Y", fs)
+	_, err := NewEq(X, Y)
+	if err == nil {
+		t.Error("Expected error for higher-order Eq")
+	}
+}
+
+func TestEqTopSort(t *testing.T) {
+	X, _ := NewVar("X", TopS)
+	S := &UninterpretedSort{Name: "S"}
+	Y, _ := NewVar("Y", S)
+	_, err := NewEq(X, Y)
+	if err != nil {
+		t.Error("Eq with TopSort should succeed")
+	}
+}
+
+func TestNotString(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	X, _ := NewVar("X", S)
+	Y, _ := NewVar("Y", S)
+	eq, _ := NewEq(X, Y)
+	notEq, _ := NewNot(eq)
+	if notEq.String() != "(X != Y)" {
+		t.Errorf("Not(Eq) String() = %q, want %q", notEq.String(), "(X != Y)")
+	}
+
+	and, _ := NewAnd(eq)
+	notAnd, _ := NewNot(and)
+	if notAnd.String() != "Not(And((X == Y)))" {
+		t.Errorf("Not(And) String() = %q, want %q", notAnd.String(), "Not(And((X == Y)))")
+	}
+}
+
+func TestNotBadSort(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	X, _ := NewVar("X", S)
+	_, err := NewNot(X) // X is sort S, not Boolean
+	if err == nil {
+		t.Error("Expected error for Not with non-Boolean")
+	}
+}
+
+func TestAndOr(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	X, _ := NewVar("X", S)
+	Y, _ := NewVar("Y", S)
+	leq := NewConst("leq", mustFS(t, S, S, Boolean))
+	app1, _ := NewApply(leq, X, Y)
+	app2, _ := NewApply(leq, Y, X)
+
+	and, err := NewAnd(app1, app2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if and.String() != "And(leq(X, Y), leq(Y, X))" {
+		t.Errorf("And String() = %q", and.String())
+	}
+
+	or, err := NewOr(app1, app2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if or.String() != "Or(leq(X, Y), leq(Y, X))" {
+		t.Errorf("Or String() = %q", or.String())
+	}
+}
+
+func TestAndBadSort(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	X, _ := NewVar("X", S) // S, not Boolean
+	_, err := NewAnd(X)
+	if err == nil {
+		t.Error("Expected error for And with non-Boolean")
+	}
+}
+
+func TestTrueFalse(t *testing.T) {
+	// True is empty And, False is empty Or
+	if True.String() != "And()" {
+		t.Errorf("True = %q, want %q", True.String(), "And()")
+	}
+	if False.String() != "Or()" {
+		t.Errorf("False = %q, want %q", False.String(), "Or()")
+	}
+}
+
+func TestImpliesIff(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	X, _ := NewVar("X", S)
+	Y, _ := NewVar("Y", S)
+	leq := NewConst("leq", mustFS(t, S, S, Boolean))
+	a, _ := NewApply(leq, X, Y)
+	b, _ := NewApply(leq, Y, X)
+
+	impl, err := NewImplies(a, b)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if impl.String() != "Implies(leq(X, Y), leq(Y, X))" {
+		t.Errorf("Implies = %q", impl.String())
+	}
+
+	iff, err := NewIff(a, b)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if iff.String() != "Iff(leq(X, Y), leq(Y, X))" {
+		t.Errorf("Iff = %q", iff.String())
+	}
+}
+
+func TestForAll(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	X, _ := NewVar("X", S)
+	Y, _ := NewVar("Y", S)
+	Z, _ := NewVar("Z", S)
+	leq := NewConst("leq", mustFS(t, S, S, Boolean))
+	leqXY, _ := NewApply(leq, X, Y)
+	leqYZ, _ := NewApply(leq, Y, Z)
+	leqXZ, _ := NewApply(leq, X, Z)
+	andTerm, _ := NewAnd(leqXY, leqYZ)
+	impl, _ := NewImplies(andTerm, leqXZ)
+
+	fa, err := NewForAll([]*Var{X, Y, Z}, impl)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("ForAll:", fa.String())
+	if !SortEqual(fa.NodeSort(), Boolean) {
+		t.Error("ForAll sort should be Boolean")
+	}
+}
+
+func TestForAllEmpty(t *testing.T) {
+	_, err := NewForAll([]*Var{}, True)
+	if err == nil {
+		t.Error("Expected error for empty variables")
+	}
+}
+
+func TestForAllBadBody(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	X, _ := NewVar("X", S)
+	_, err := NewForAll([]*Var{X}, X) // body is sort S
+	if err == nil {
+		t.Error("Expected error for non-Boolean body")
+	}
+}
+
+func TestExists(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	X, _ := NewVar("X", S)
+	eq, _ := NewEq(X, X)
+	ex, err := NewExists([]*Var{X}, eq)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("Exists:", ex.String())
+}
+
+func TestLambda(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	X, _ := NewVar("X", S)
+	eq, _ := NewEq(X, X)
+	lam, err := NewLambda([]*Var{X}, eq)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("Lambda:", lam.String())
+}
+
+func TestIte(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	X, _ := NewVar("X", S)
+	Y, _ := NewVar("Y", S)
+	cond, _ := NewEq(X, Y)
+	ite, err := NewIte(cond, X, Y)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if ite.String() != "Ite((X == Y), X, Y)" {
+		t.Errorf("Ite String() = %q", ite.String())
+	}
+	if !SortEqual(ite.NodeSort(), S) {
+		t.Errorf("Ite sort = %s, want S", ite.NodeSort())
+	}
+}
+
+func TestIteBadCond(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	X, _ := NewVar("X", S)
+	_, err := NewIte(X, X, X) // cond is S, not Boolean
+	if err == nil {
+		t.Error("Expected error for non-Boolean condition")
+	}
+}
+
+func TestGloballyEventually(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	X, _ := NewVar("X", S)
+	Y, _ := NewVar("Y", S)
+	eq, _ := NewEq(X, Y)
+
+	g, err := NewGlobally(nil, eq)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if g.String() != "globally((X == Y))" {
+		t.Errorf("Globally = %q", g.String())
+	}
+
+	env := "env1"
+	e, err := NewEventually(&env, eq)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if e.String() != "eventually[env1]((X == Y))" {
+		t.Errorf("Eventually = %q", e.String())
+	}
+}
+
+func TestWhenOperator(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	X, _ := NewVar("X", S)
+	cond, _ := NewEq(X, X)
+	w, err := NewWhenOperator("when", X, cond)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if w.String() != "WhenOperator(when,X,(X == X))" {
+		t.Errorf("WhenOperator = %q", w.String())
+	}
+	if !SortEqual(w.NodeSort(), S) {
+		t.Error("WhenOperator sort should be S")
+	}
+}
+
+func TestCond(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	X, _ := NewVar("X", S)
+	cond, _ := NewEq(X, X)
+	c, err := NewCond(cond, X)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if c.String() != "Cond((X == X), X)" {
+		t.Errorf("Cond = %q", c.String())
+	}
+}
+
+func TestNamedBinder(t *testing.T) {
+	X, _ := NewVar("X", TopS)
+	Y, _ := NewVar("Y", TopS)
+	Z, _ := NewVar("Z", TopS)
+
+	f := NewConst("f", mustFS(t, TopS, TopS, Boolean))
+	fXY, _ := NewApply(f, X, Y)
+	fXZ, _ := NewApply(f, X, Z)
+	andTerm, _ := NewAnd(fXY, fXZ)
+	eqYZ, _ := NewEq(Y, Z)
+	impl, _ := NewImplies(andTerm, eqYZ)
+
+	b, err := NewNamedBinder("mybinder", []*Var{X, Y, Z}, nil, impl)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("NamedBinder:", b.String())
+	t.Log("NamedBinder sort:", b.NodeSort().String())
+
+	// Sort should be TopSort * TopSort * TopSort -> Boolean
+	fs, ok := b.NodeSort().(*FunctionSort)
+	if !ok {
+		t.Fatalf("Expected FunctionSort, got %T", b.NodeSort())
+	}
+	if fs.Arity() != 3 {
+		t.Errorf("Arity = %d, want 3", fs.Arity())
+	}
+}
+
+func TestNamedBinderNoVars(t *testing.T) {
+	X, _ := NewVar("X", TopS)
+	S := &UninterpretedSort{Name: "S"}
+	Z, _ := NewVar("Z", S)
+
+	b, err := NewNamedBinder("mybinder", []*Var{X, Y_(), Z}, nil, Z)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("NamedBinder:", b.String())
+	t.Log("NamedBinder sort:", b.NodeSort().String())
+}
+
+func TestNamedBinderCall(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	X, _ := NewVar("X", S)
+	eq, _ := NewEq(X, X)
+	nb, _ := NewNamedBinder("nb", []*Var{X}, nil, eq)
+
+	// Call with no args returns self
+	result, err := nb.Call()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if result != nb {
+		t.Error("Call with no args should return self")
+	}
+}
+
+func TestFormulaChildren(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	X, _ := NewVar("X", S)
+	eq, _ := NewEq(X, X)
+
+	// ForAll children should be [body] only, not variables
+	fa, _ := NewForAll([]*Var{X}, eq)
+	children := fa.Children()
+	if len(children) != 1 {
+		t.Errorf("ForAll Children() len = %d, want 1", len(children))
+	}
+}
+
+func TestFormulaEquality(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	X, _ := NewVar("X", S)
+	Y, _ := NewVar("Y", S)
+
+	eq1, _ := NewEq(X, Y)
+	eq2, _ := NewEq(X, Y)
+	if !eq1.Equal(eq2) {
+		t.Error("Same Eq nodes should be equal")
+	}
+
+	not1, _ := NewNot(eq1)
+	not2, _ := NewNot(eq2)
+	if !not1.Equal(not2) {
+		t.Error("Same Not nodes should be equal")
+	}
+}
+
+// Y_ helper for creating Y variable with TopSort
+func Y_() *Var {
+	v, _ := NewVar("Y", TopS)
+	return v
+}
+
+// Python __main__ antisymmetric example
+func TestAntisymmetric(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	X, _ := NewVar("X", S)
+	Y, _ := NewVar("Y", S)
+	leq := NewConst("leq", mustFS(t, S, S, Boolean))
+
+	leqXY, _ := NewApply(leq, X, Y)
+	leqYX, _ := NewApply(leq, Y, X)
+	andTerm, _ := NewAnd(leqXY, leqYX)
+	eqYX, _ := NewEq(Y, X)
+	impl, _ := NewImplies(andTerm, eqYX)
+	antisym, err := NewForAll([]*Var{X, Y}, impl)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("antisymmetric:", antisym.String())
+}
+
+func FuzzAndConstruction(f *testing.F) {
+	f.Add(0)
+	f.Add(1)
+	f.Add(5)
+	f.Fuzz(func(t *testing.T, n int) {
+		if n < 0 || n > 20 {
+			return
+		}
+		S := &UninterpretedSort{Name: "S"}
+		X, _ := NewVar("X", S)
+		Y, _ := NewVar("Y", S)
+		eq, _ := NewEq(X, Y)
+
+		terms := make([]Node, n)
+		for i := range terms {
+			terms[i] = eq
+		}
+		and, err := NewAnd(terms...)
+		if err != nil {
+			t.Fatalf("Unexpected error: %v", err)
+		}
+		if len(and.Children()) != n {
+			t.Errorf("Children len = %d, want %d", len(and.Children()), n)
+		}
+	})
+}
---
+++ logic/term_test.go
@@ -0,0 +1,248 @@
+package logic
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestVarValid(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	v, err := NewVar("X", S)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if v.String() != "X" {
+		t.Errorf("String() = %q, want %q", v.String(), "X")
+	}
+	if !SortEqual(v.NodeSort(), S) {
+		t.Error("Sort should be S")
+	}
+}
+
+func TestVarInvalidName(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	_, err := NewVar("x", S)
+	if err == nil {
+		t.Error("Expected error for lowercase variable name")
+	}
+	var ivyErr *IvyError
+	if !errors.As(err, &ivyErr) {
+		t.Error("Expected IvyError")
+	}
+}
+
+func TestVarEmpty(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	_, err := NewVar("", S)
+	if err == nil {
+		t.Error("Expected error for empty variable name")
+	}
+}
+
+func TestVarEqual(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	v1, _ := NewVar("X", S)
+	v2, _ := NewVar("X", S)
+	v3, _ := NewVar("Y", S)
+	if !v1.Equal(v2) {
+		t.Error("Same vars should be equal")
+	}
+	if v1.Equal(v3) {
+		t.Error("Different vars should not be equal")
+	}
+}
+
+func TestConst(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	c := NewConst("leq", mustFS(t, S, S, Boolean))
+	if c.String() != "leq" {
+		t.Errorf("String() = %q, want %q", c.String(), "leq")
+	}
+}
+
+func TestApplyValid(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	X, _ := NewVar("X", S)
+	Y, _ := NewVar("Y", S)
+	leq := NewConst("leq", mustFS(t, S, S, Boolean))
+
+	app, err := NewApply(leq, X, Y)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if app.String() != "leq(X, Y)" {
+		t.Errorf("String() = %q, want %q", app.String(), "leq(X, Y)")
+	}
+	if !SortEqual(app.NodeSort(), Boolean) {
+		t.Errorf("Sort should be Boolean, got %s", app.NodeSort())
+	}
+}
+
+func TestApplyArityMismatch(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	X, _ := NewVar("X", S)
+	leq := NewConst("leq", mustFS(t, S, S, Boolean))
+
+	_, err := NewApply(leq, X) // expects 2 args, got 1
+	if err == nil {
+		t.Error("Expected arity error")
+	}
+	var sortErr *SortError
+	if !errors.As(err, &sortErr) {
+		t.Errorf("Expected SortError, got %T", err)
+	}
+}
+
+func TestApplySortMismatch(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	T := &UninterpretedSort{Name: "T"}
+	X, _ := NewVar("X", S)
+	Y, _ := NewVar("Y", T)
+	leq := NewConst("leq", mustFS(t, S, S, Boolean))
+
+	_, err := NewApply(leq, X, Y) // Y is T, expects S
+	if err == nil {
+		t.Error("Expected sort mismatch error")
+	}
+}
+
+func TestApplyTopSortPassthrough(t *testing.T) {
+	X, _ := NewVar("X", TopS)
+	Y, _ := NewVar("Y", TopS)
+	h := NewConst("h", TopS)
+
+	app, err := NewApply(h, X, Y)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !SortEqual(app.NodeSort(), TopS) {
+		t.Error("Apply with TopSort func should have TopSort result")
+	}
+}
+
+func TestApplyNonFunction(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	X, _ := NewVar("X", S)
+	c := NewConst("c", S) // not a function sort
+
+	_, err := NewApply(c, X)
+	if err == nil {
+		t.Error("Expected error for non-function application")
+	}
+}
+
+func TestVarCall(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	fs := mustFS(t, S, Boolean)
+	V, _ := NewVar("V", fs)
+	X, _ := NewVar("X", S)
+
+	// Call with args
+	result, err := V.Call(X)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if result.String() != "V(X)" {
+		t.Errorf("String() = %q, want %q", result.String(), "V(X)")
+	}
+
+	// Call with no args returns self
+	result, err = V.Call()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if result != V {
+		t.Error("Call() with no args should return self")
+	}
+}
+
+func TestConstCall(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	leq := NewConst("leq", mustFS(t, S, S, Boolean))
+	X, _ := NewVar("X", S)
+	Y, _ := NewVar("Y", S)
+
+	result, err := leq.Call(X, Y)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if result.String() != "leq(X, Y)" {
+		t.Errorf("String() = %q, want %q", result.String(), "leq(X, Y)")
+	}
+}
+
+func TestApplyEqual(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	X, _ := NewVar("X", S)
+	Y, _ := NewVar("Y", S)
+	leq := NewConst("leq", mustFS(t, S, S, Boolean))
+
+	a1, _ := NewApply(leq, X, Y)
+	a2, _ := NewApply(leq, X, Y)
+	if !a1.Equal(a2) {
+		t.Error("Same applies should be equal")
+	}
+}
+
+func TestApplyChildren(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	X, _ := NewVar("X", S)
+	Y, _ := NewVar("Y", S)
+	leq := NewConst("leq", mustFS(t, S, S, Boolean))
+	app, _ := NewApply(leq, X, Y)
+
+	children := app.Children()
+	if len(children) != 3 { // func + 2 terms
+		t.Errorf("Children() len = %d, want 3", len(children))
+	}
+}
+
+// Reproduce Python __main__ example
+func TestPythonExample(t *testing.T) {
+	S := &UninterpretedSort{Name: "S"}
+	X, _ := NewVar("X", S)
+	Y, _ := NewVar("Y", S)
+	Z, _ := NewVar("Z", S)
+
+	BinRel := mustFS(t, S, S, Boolean)
+	leq := NewConst("leq", BinRel)
+
+	leqXY, _ := NewApply(leq, X, Y)
+	leqYZ, _ := NewApply(leq, Y, Z)
+	leqXZ, _ := NewApply(leq, X, Z)
+
+	// transitive1: ForAll (X,Y,Z). Implies(And(leq(X,Y), leq(Y,Z)), leq(X,Z))
+	andTerm, _ := NewAnd(leqXY, leqYZ)
+	impl, _ := NewImplies(andTerm, leqXZ)
+	trans1, err := NewForAll([]*Var{X, Y, Z}, impl)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !SortEqual(trans1.NodeSort(), Boolean) {
+		t.Error("ForAll sort should be Boolean")
+	}
+	t.Log("transitive1:", trans1.String())
+}
+
+func FuzzVarName(f *testing.F) {
+	f.Add("X")
+	f.Add("x")
+	f.Add("")
+	f.Add("123")
+	f.Fuzz(func(t *testing.T, name string) {
+		S := &UninterpretedSort{Name: "S"}
+		v, err := NewVar(name, S)
+		if len(name) == 0 || name[0] < 'A' || name[0] > 'Z' {
+			if err == nil {
+				t.Error("Expected error for invalid name")
+			}
+		} else {
+			if err != nil {
+				t.Errorf("Unexpected error for name %q: %v", name, err)
+			}
+			if v.String() != name {
+				t.Errorf("String() = %q, want %q", v.String(), name)
+			}
+		}
+	})
+}
