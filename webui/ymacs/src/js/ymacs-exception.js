c83fe0c6e9c6e93d024fa9991041cf9940dfa00a
Jason E. Aten, Ph.D. <jason@devnull>
2026-03-16 08:24:49 -0300

    latest

--- webui/ymacs/src/js/ymacs-exception.js
+++
@@ -1,7 +0,0 @@
-/// This file is part of Ymacs - www.ymacs.org
-/// Copyright (c) 2009-2024 Mihai Bazon <mihai.bazon@gmail.com>
-/// License: MIT
-
-export function Ymacs_Exception(why) {
-    this.message = why;
-};
