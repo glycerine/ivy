bc67bf4243e9250260ed13ee011c396df2fe211b
Jason E. Aten, Ph.D. <jason@devnull>
2026-03-23 23:50:05 -0300

    Canonical has Blake3 method

--- ivyutils/canon.go
+++ ivyutils/canon.go
@@ -1,5 +1,10 @@
 package ivyutils
 
+import (
+	cristalbase64 "github.com/cristalhq/base64"
+	"github.com/glycerine/blake3"
+)
+
 // Canonical strings are 1:1 with the state
 // they represent, so immune to internal collection
 // reordering differences: the are s-expressions
@@ -11,4 +16,17 @@ type Canonizer interface {
 	// Canon returns a canonical S-expression string
 	// for the given implementor.
 	Canon() Canonical
+}
+
+// Blake3 returns the first 33 bytes of
+// 64 byte (512 bit) blake3 hash of a Canonical string.
+// The hash is then base-64 URL encoded,
+// and prefixed with "blake3.33B-".
+// It is goroutine safe and lock free, since
+// it creates a new hasher every time.
+func (c Canonical) Blake3() string {
+	h := blake3.New(64, nil)
+	h.Write([]byte(c))
+	sum := h.Sum(nil)
+	return "blake3.33B-" + cristalbase64.URLEncoding.EncodeToString(sum[:33])
 }
