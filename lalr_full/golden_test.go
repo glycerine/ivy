20bebe63dfd207d48edd44e97ed69696cb470c88
Jason E. Aten, Ph.D. <jason@devnull>
2026-03-28 14:34:38 -0300

    golden 127430

--- lalr_full/golden_test.go
+++ lalr_full/golden_test.go
@@ -535,12 +535,12 @@ func ordLiveCompare(t *testing.T, verbose, diffStop bool) {
 				if i > 10 {
 					fmt.Printf("(omit prior matching xtrace from 0 - %v, for speed...)\n", i-n)
 				}
-				// note: truncate to first 100 bytes to
+				// note: truncate to first 300 bytes to
 				// avoid regurgitating very long canonical
 				// strings for modules of matching stuff.
 				for j, pys := range pyLast10 {
-					fmt.Printf("%06d  go : %.100s", i-n+j+1, goLast10[j])
-					fmt.Printf("        py : %.100s\n", pys)
+					fmt.Printf("%06d  go : %.300s", i-n+j+1, goLast10[j])
+					fmt.Printf("        py : %.300s\n", pys)
 				}
 			}
 			// If both lines are HASH lines with canon= data, show a structured diff.
