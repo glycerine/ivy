9669e148629b868d64ccaa7353a10c122e155a5e
Jason E. Aten, Ph.D. <jason@devnull>
2026-04-14 02:56:45 -0300

    bah. fix python

--- pyivy/ivy/ivy/ivy_l2s.py
+++ pyivy/ivy/ivy/ivy_l2s.py
@@ -1228,7 +1228,7 @@ def l2s_tactic_int(prover,goals,proof,tactic_name):
             actual_returns = stmt.args[1:]
             if __debug__:
                 for _ri, _sym in enumerate(actual_returns):
-                    xtracer.trace("l2s.SharedStep7 instrStmt.monitor return[%d] type=%s name=%s inSP=%s inSWh=%s inSWa=%s" % (_ri, type(_sym).__name__, _ri.canon(), _sym in symprops, _sym in symwhens, _sym in symwaits))
+                    xtracer.trace("l2s.SharedStep7 instrStmt.monitor return[%d] type=%s name=%s inSP=%s inSWh=%s inSWa=%s" % (_ri, type(_sym).__name__, _sym, _sym in symprops, _sym in symwhens, _sym in symwaits))
             if any(sym in symprops or sym in symwhens
                    or sym in symwaits for sym in actual_returns):
                 return instr_stmt(stmt.split_returns(),labels)
