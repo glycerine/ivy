f49239e72bb5129218668e38dd7c2709855dd8cc
Jason E. Aten, Ph.D. <jason@devnull>
2026-04-17 15:42:53 -0300

    onetime.sh install abd and aigtoaig

---
+++ pyivy/onetime.sh
@@ -0,0 +1,23 @@
+## these command line utilities are called from 
+## python liveness-to-safety checks, and must be 
+## pre-installed to the bin directories to be found.
+## they are vendored in the ivy/submodules/ dir.
+
+mkdir -p ivy/bin
+mkdir -p ivy/ivy/bin
+cd ivy/submodules/aiger
+gcc -O2 -o aigtoaig aigtoaig.c aiger.c
+
+## install to both bin directories, since
+## ivy_mc.py:1721 constructs the path:
+##   aigtoaig_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'bin', 'aigtoaig')
+## --> ~/ivy/pyivy/ivy/ivy/bin/aigtoaig
+cp -p aigtoaig ../../bin/
+cp -p aigtoaig ../../ivy/bin/
+cd ../../..
+
+cd ivy/submodules/abc
+make -j4 ## it takes a few minutes to compile binary 'abc'.
+cp -p abc ../../bin/
+cp -p abc ../../ivy/bin/
+cd ../../..
