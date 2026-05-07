484f843e45daddef87809ffb626a86fd03f0ecbe
Jason E. Aten, Ph.D. <jason@devnull>
2026-05-07 20:59:25 -0300

    update z3vendor/z3/build-wasm-api.sh to produce z3-471-api.wasm

--- goivy/z3vendor/z3/build-wasm-api.sh
+++ goivy/z3vendor/z3/build-wasm-api.sh
@@ -19,5 +19,5 @@ emcc \
 
 echo "Built build/z3-api.js and build/z3-api.wasm"
 
-cp -p build/z3-api.wasm ../../webvue/static/
-cp -p build/z3-api.js   ../../webvue/static/
+cp -p build/z3-api.wasm ../../webvue/static/z3-471-api.wasm
+cp -p build/z3-api.js   ../../webvue/static/z3-471-api.js
