2583524e27835135d27e88bf9692bf02e72a9812
Jason E. Aten, Ph.D. <jason@devnull>
2026-05-07 21:12:34 -0300

    same 471 detail on backup wasm/js

--- goivy/z3vendor/z3/compile-wasm.sh
+++ goivy/z3vendor/z3/compile-wasm.sh
@@ -18,3 +18,8 @@ ln -s wasm.build build
 # save them above in case we rebuild native and wipe them out by accident.
 for i in z3-api.js z3-api.wasm z3.wasm libz3.a libz3.wasm libz3.dylib; do cp -p wasm.build/$i ../wasm_lib/; done
 
+# make the names match what build-wasm-api.sh copies into ../../webvue/static/
+cd ../wasm_lib
+mv z3-api.wasm z3-471-api.wasm
+mv z3-api.js   z3-471-api.js
+cd ..
