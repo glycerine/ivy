e5847aefdd3c8cc2cf34113d5d9449f7e6a6278c
Jason E. Aten, Ph.D. <jason@devnull>
2026-05-09 09:17:11 -0300

    turn off gc on every xtrace

--- goivy/xtracer/xtracer.go
+++ goivy/xtracer/xtracer.go
@@ -60,11 +60,11 @@ func trace(format string, args ...interface{}) {
 		return
 	}
 
-	//if globalXtraceCounter%1000 == 0 {
-	runtime.ReadMemStats(mems)
-	fmt.Printf("[at trace %v] mems.HeapAlloc = %0.3f MB; HeapInuse = %0.3f MB\n", globalXtraceCounter, float64(mems.HeapAlloc)/(1<<20), float64(mems.HeapInuse)/(1<<20))
-	maybePaceGC(globalXtraceCounter, mems)
-	//}
+	if globalXtraceCounter%1000 == 0 {
+		runtime.ReadMemStats(mems)
+		fmt.Printf("[at trace %v] mems.HeapAlloc = %0.3f MB; HeapInuse = %0.3f MB\n", globalXtraceCounter, float64(mems.HeapAlloc)/(1<<20), float64(mems.HeapInuse)/(1<<20))
+		maybePaceGC(globalXtraceCounter, mems)
+	}
 	//maybeWriteHeapProfile(globalXtraceCounter)
 	globalXtraceCounter++
 
