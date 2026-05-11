0ba4f37d5281ef85ab1e501faf793bdb85de315c
Jason E. Aten, Ph.D. <jason@devnull>
2026-05-10 21:40:20 -0300

    crashing on main.runtime.alloc

--- goivy/file_probe.go
+++ goivy/file_probe.go
@@ -8,8 +8,5 @@ import (
 
 func ivyReadableFileExists(filename string) bool {
 	info, err := fileops.Stat(filename)
-	if err != nil {
-		fmt.Printf("fileops.Stat(filename='%v') gave err='%v'\n", filename, err)
-	}
 	return err == nil && !info.IsDir
 }
