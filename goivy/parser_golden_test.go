ae77fcda78275ad9aa822a2b020d821ba18b324c
Jason E. Aten, Ph.D. <jason@devnull>
2026-05-10 22:40:57 -0300

    parser build seems to link now with 4gb but still runtime out of memory almost immediately

--- goivy/parser_golden_test.go
+++ goivy/parser_golden_test.go
@@ -1296,7 +1296,13 @@ func tinynode_ivy_check_xtrace(t *testing.T, args []string, ivyFile, repo string
 	goivyWasm := filepath.Join(goivyRoot, "webvue", "static", "goivy-check-tinygo-js.wasm")
 	goBinary := filepath.Join(runtime.GOROOT(), "bin", "go")
 	tinygoBinary := filepath.Join("/usr", "local", "bin", "tinygo")
-	tinygoFlags := []string{"-panic=print", "-gc=precise", "-no-debug", `-ldflags=-extldflags=--initial-memory=4294967296 --stack-first -z stack-size=2097152`}
+	tinygoFlags := []string{
+		"-panic=print",
+		"-gc=precise",
+		"-no-debug",
+		`-ldflags`,
+		`-extldflags="--initial-memory=4294967296 --stack-first -z stack-size=2097152"`,
+	}
 
 	// GOOS=js GOARCH=wasm /usr/local/bin/tinygo build -panic=trap -gc=precise -no-debug -o webvue/static/goivy-check-tinygo-js.wasm ./cmd/goivy_check_jswasm/
 
