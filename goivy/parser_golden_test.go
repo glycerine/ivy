fe67458e42e8b10a1f36997a375cfc1a4d94d2ed
Jason E. Aten, Ph.D. <jason@devnull>
2026-05-10 22:13:52 -0300

    parser_golden_test tinygo flags were not keeping up with Makefile

--- goivy/parser_golden_test.go
+++ goivy/parser_golden_test.go
@@ -1296,7 +1296,7 @@ func tinynode_ivy_check_xtrace(t *testing.T, args []string, ivyFile, repo string
 	goivyWasm := filepath.Join(goivyRoot, "webvue", "static", "goivy-check-tinygo-js.wasm")
 	goBinary := filepath.Join(runtime.GOROOT(), "bin", "go")
 	tinygoBinary := filepath.Join("/usr", "local", "bin", "tinygo")
-	tinygoFlags := []string{"-panic=trap", "-gc=precise", "-no-debug"}
+	tinygoFlags := []string{"-panic=trap", "-gc=precise", "-no-debug", `-ldflags="-extldflags='--initial-memory=4294967296 --stack-first -z stack-size=2097152'"`}
 
 	// GOOS=js GOARCH=wasm /usr/local/bin/tinygo build -panic=trap -gc=precise -no-debug -o webvue/static/goivy-check-tinygo-js.wasm ./cmd/goivy_check_jswasm/
 	wasmFullCmd := fmt.Sprintf("cd %v && GOOS=js GOARCH=wasm %v build %v -o %v ./cmd/goivy_check_jswasm", goivyRoot, tinygoBinary, strings.Join(tinygoFlags, " "), goivyWasm)
