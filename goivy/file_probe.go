2ddcb55f424a9659e62378b012f8a17e9baa2265
Jason E. Aten, Ph.D. <jason@devnull>
2026-05-10 21:12:48 -0300

    elim segfault print

--- goivy/file_probe.go
+++ goivy/file_probe.go
@@ -8,6 +8,8 @@ import (
 
 func ivyReadableFileExists(filename string) bool {
 	info, err := fileops.Stat(filename)
-	fmt.Printf("fileops.Stat(filename='%v') gave err='%v' and info='%v'\n", filename, err, info)
+	if err != nil {
+		fmt.Printf("fileops.Stat(filename='%v') gave err='%v'\n", filename, err)
+	}
 	return err == nil && !info.IsDir
 }
