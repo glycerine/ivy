953015d18752f8dcf9bc1eec4160349d3d05a88d
Jason E. Aten, Ph.D. <jason@devnull>
2026-05-22 16:52:25 -0300

    more matching at actions_update.go:1872

--- goivy/actions_update.go
+++ goivy/actions_update.go
@@ -1870,7 +1870,7 @@ func (a *LogicCallAction) IntUpdate(ctx *UpdateContext) *Update {
 	defer xtracer.Trace("actions.CallAction.int_update EXIT")
 	calleeName := constName(a.Callee)
 	if xtracer.Enabled {
-		fmt.Printf("DIAG CallAction.IntUpdate callee=%v\n", calleeName)
+		fmt.Printf("DIAG CallAction.int_update callee=%v\n", calleeName)
 	}
 	if calleeName == "" {
 		// Python (ivy_actions.py:1318): name = self.args[0].rep — would
