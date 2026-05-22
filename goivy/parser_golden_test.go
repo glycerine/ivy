44adc49cfadf6425104e68ab574d32be97e2f51a
Jason E. Aten, Ph.D. <jason@devnull>
2026-05-22 10:41:42 -0300

    go not handling sorry proof tactic

--- goivy/parser_golden_test.go
+++ goivy/parser_golden_test.go
@@ -566,7 +566,7 @@ func TestGoldenAll(t *testing.T) {
 
 	// already green once specs should not be revisted until we
 	// get through the others, to save time.
-	skipTo := 3
+	skipTo := 8
 
 	skipRebuild := false
 	for ipath, path := range paths {
