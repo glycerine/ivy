9a74b94b8e5a5de3e4ae3a34a07b89776b949685
Jason E. Aten, Ph.D. <jason@devnull>
2026-05-24 19:42:54 -0300

    fix next div, include proof_tactics_test.go

---
+++ goivy/proof_tactics_test.go
@@ -0,0 +1,26 @@
+package goivy
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestLetTacticCompilesDefinitionsAsEqualityAtoms(t *testing.T) {
+	cfg := NewAstConfig()
+	mod := New()
+	pc := NewProofChecker(nil, mod, nil, nil, nil, cfg)
+	goal := cfg.NewLabeledFormula(cfg.NewAtom("asrt1"), True)
+	def := cfg.NewDefinition(cfg.NewVariable("X", "S"), cfg.NewAtom("x"))
+	proof := cfg.NewLetTactic([]Node{def})
+
+	out := captureActionUpdateStdout(t, func() {
+		_, _ = pc.letTactic([]*LabeledFormula{goal}, proof)
+	})
+
+	if !strings.Contains(out, "XTRACE: proof.CompileExprVocab ENTER exprType=Atom\n") {
+		t.Fatalf("let tactic did not compile an equality Atom:\n%s", out)
+	}
+	if strings.Contains(out, "XTRACE: proof.CompileExprVocab ENTER exprType=Definition\n") {
+		t.Fatalf("let tactic compiled the Definition node directly:\n%s", out)
+	}
+}
