e46f1bd3ccfde411abcefac83ad81dc6188a4d80
Jason E. Aten, Ph.D. <jason@devnull>
2026-05-25 21:23:47 -0300

    start to add chacha8c.hpp

--- goivy/ivy2cpp/repl.go
+++ goivy/ivy2cpp/repl.go
@@ -639,6 +639,11 @@ func (g *Generator) emitMainParamSetup(w *cppWriter) {
 	}
 	// argv parsing loop: key=value -> param assignment OR special key.
 	w.line("int seed = 1;")
+
+	w.line("std::uint8_t seed32[c8::key_size];")
+	w.line(`std::memcpy(seed32, "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456", chacha8c::key_size);`)
+	w.line("__chacha8c_rng = chacha8c::NewChaCha8(seed32);")
+
 	w.line("int sleep_ms = 10;")
 	w.line("int final_ms = 0;")
 	if g.Config.Target != "test" {
--- goivy/ivy2cpp/runtime.go
+++ goivy/ivy2cpp/runtime.go
@@ -186,6 +186,9 @@ func (g *Generator) emitRuntimeImplPreamble(w *cppWriter) {
 	w.line("std::ofstream __ivy_modelfile;")
 	w.line("void __ivy_exit(int code){exit(code);}")
 	w.line(`#include "ivy_threads.hpp"`)
+	w.line(`#include "chacha8.hpp"`)
+	w.line("chacha8c::ChaCha8* __chacha8c_rng;")
+
 	w.blank()
 }
 
