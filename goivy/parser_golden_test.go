db5b8dbb59b44e26505b268eb4ec76eae8e93db7
Jason E. Aten, Ph.D. <jason@devnull>
2026-05-27 13:59:53 -0300

    make golden-all only on v1.6 stops at 123 of 184

--- goivy/parser_golden_test.go
+++ goivy/parser_golden_test.go
@@ -589,7 +589,7 @@ func TestGoldenAll(t *testing.T) {
 
 	// 243 runs for 500 seconds:
 	// XTRACE_OFF=1 time python -O `which ivy_check` isolate=this /Users/jaten/ivy/ivy-lang-examples/test/flash2_finite_mc.ivy
-	skipTo := 31 // 243 runs for 500 seconds.
+	skipTo := 0 // 243 runs for 500 seconds.
 	if skipTo == 31 {
 		startingIsolate = "this"
 	}
