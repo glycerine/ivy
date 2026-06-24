80119c6ed470edc4a435887311e7a6fdf1a98131
Jason E. Aten, Ph.D. <jason@devnull>
2026-06-24 20:49:40 -0300

    iter for coro

--- gojr/src/intrinsicPackages.ts
+++ gojr/src/intrinsicPackages.ts
@@ -1,5 +1,6 @@
 export function isIntrinsicPackageImport(importPath: string): boolean {
-  return importPath === "runtime" ||
+  return importPath === "iter" ||
+    importPath === "runtime" ||
     importPath === "syscall/js" ||
     importPath === "unsafe" ||
     importPath === "internal/reflectlite";
