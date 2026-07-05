38fe7765c53de6a922f426c4486a1848fb88c8de
Jason E. Aten, Ph.D. <jason@devnull>
2026-07-05 20:06:59 -0300

    delete _control server

--- goivy/_control/admin/postgres/00_bootstrap_databases.sql
+++
@@ -1,6 +0,0 @@
--- Local bootstrap script. Run with a PostgreSQL role that can create databases.
--- The development bootstrap role is jaten/jaten.
-
-SELECT 'CREATE DATABASE ivyvue'
-WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'ivyvue')
-\gexec
